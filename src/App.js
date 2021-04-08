058e5711b759a65dd73c52cdf672648750cffe11
kmork <knut.mork@gmail.com>
2021-04-08 22:10:17 +0200

    Remove spinning

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Silly Wheelie - rolling to a street near you!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

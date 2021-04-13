145aabbbe5cd8a3b9e6d323d4ad3f34408847184
kmork <knut.mork@gmail.com>
2021-04-13 20:59:55 +0200

    More stats

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -6,35 +6,51 @@ const Stats=()=>{
             <header className="App-header">
                 <h1>Statistics</h1>
                 <table>
-                    <tr>
+                    <tr align="left">
                         <td>Total cycling time:</td>
                         <td>?</td>
                     </tr>
-                    <tr>
+                    <tr align="left">
                         <td>Total distance:</td>
                         <td>?</td>
                     </tr>
-                    <tr>
+                    <tr align="left">
                         <td>Number of bikes:</td>
                         <td>1</td>
                     </tr>
-                    <tr>
+                    <tr align="left">
                         <td>Number of falls:</td>
                         <td>4</td>
                     </tr>
-                    <tr>
+                    <tr align="left">
+                        <td>Number of jumps:</td>
+                        <td>0</td>
+                    </tr>
+                    <tr align="left">
                         <td>Average speed:</td>
                         <td>?</td>
                     </tr>
-                    <tr>
+                    <tr align="left">
                         <td>Average km per round:</td>
                         <td>?</td>
                     </tr>
-                    <tr>
-                        <td>Average number of bystanders commenting:</td>
-                        <td>4</td>
+                    <tr align="left">
+                        <td>Max speed:</td>
+                        <td>12,5 km/h</td>
                     </tr>
-                    <tr>
+                    <tr align="left">
+                        <td>Max ride:</td>
+                        <td>33,4 km</td>
+                    </tr>
+                    <tr align="left">
+                        <td>Babies hit & run:</td>
+                        <td>0</td>
+                    </tr>
+                    <tr align="left">
+                        <td>Avg num of bystanders commenting:</td>
+                        <td>6</td>
+                    </tr>
+                    <tr align="left">
                         <td>Longest distance before jumping off:</td>
                         <td>5 km</td>
                     </tr>
